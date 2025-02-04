fd9ab07977f66fd18b3660f4cc702548d8627986
Guilherme <guilhermeggr23@gmail.com>
2025-02-04 17:54:10 -0300

    update: Remoção do NgOptimizedImage não usado

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -5,7 +5,7 @@ import { ButtonComponent } from "../../buttons/button/button.component";
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage, ButtonComponent],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   encapsulation: ViewEncapsulation.None,
